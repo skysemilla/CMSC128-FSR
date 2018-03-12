391a93e0cc625ad55d4865a0945d692a043a7b76
markkevien <mlcielo@up.edu.ph>
2018-03-12 14:31:46 +0800

    signUp page, not yet done

---
+++ src/components/SignUp.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Form, Button, Container, Tab, Segment, Image, Label, Icon, Divider, Grid, Menu } from 'semantic-ui-react'
+import ACSSLogo from '../assets/logo.png';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+    }
+  }
+
+  render(){
+    return(
+      <Container style={{marginTop: '5%' }}>
+      <div>
+        <div class="ui attached message">
+          <div class="content">
+            <div class="header">Welcome to our site!</div>
+            <p>Fill out the form below to sign-up for a new account</p>
+          </div>
+        </div>
+        <form class="ui form attached fluid segment">
+          <div class="equal width fields">
+            <div class="field">
+              <label>First Name</label>
+              <div class="ui fluid input">
+                <input type="text" placeholder="First Name" />
+              </div>
+            </div>
+          <div class="field">
+            <label>Last Name</label>
+            <div class="ui fluid input">
+              <input type="text" placeholder="Last Name" />
+            </div>
+          </div>
+        </div>
+        <div class="field">
+      <label>Username</label>
+      <div class="ui input">
+        <input type="text" placeholder="Username" />
+      </div>
+    </div>
+    <div class="field">
+      <label>Password</label>
+      <div class="ui input">
+        <input type="password" />
+      </div>
+    </div>
+    <div class="inline field">
+      <div class="ui checkbox">
+        <input type="checkbox" class="hidden" readonly="" tabindex="0" />
+        <label>I agree to the terms and conditions</label>
+      </div>
+    </div>
+    <button class="ui blue button" role="button">Submit</button>
+  </form>
+  <div class="ui warning bottom attached message">
+    <i aria-hidden="true" class="help icon"></i>Already signed up? <a href="#">Login here</a> instead.</div>
+</div>
+</Container>
+    );
+  }
+}
+export default App;
