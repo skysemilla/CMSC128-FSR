c9321d763023d437c5b38ffcdbf628d24f100265
cjldadios <cjldadios.uplb@gmail.com>
2018-03-16 22:08:55 +0800

    updated src/app/App.js to cater for src/features/studyload/EditStudyLoad.js and src/features/teachingload/EditTeachingLoad.js

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,9 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './../features/auth/Login';
 import Signup from './../features/auth/Signup';
 import AddTeachingLoad from './../features/teachingload/AddTeachingLoad';
+import EditTeachingLoad from './../features/teachingload/EditTeachingLoad';
 import AddStudyLoad from './../features/studyload/AddStudyLoad';
+import EditStudyLoad from './../features/studyload/EditStudyLoad';
 
 class App extends Component {
   render() {
@@ -17,7 +19,9 @@ class App extends Component {
             <Route exact={true} path="/" component={Login}/>
             <Route exact={true} path="/signup" component={Signup}/>
             <Route exact={true} path="/teachingload/add" component={AddTeachingLoad}/>
+            <Route exact={true} path="/teachingload/edit" component={EditTeachingLoad}/>
             <Route exact={true} path="/studyload/add" component={AddStudyLoad}/>
+            <Route exact={true} path="/studyload/edit" component={EditStudyLoad}/>
           </div>
         </Router>
       </div>
