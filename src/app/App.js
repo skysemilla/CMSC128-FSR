f9be9724cc2cff01b5119fbc8d3c62bbf7b61613
cjldadios <cjldadios.uplb@gmail.com>
2018-03-16 21:06:13 +0800

    updated EditTeachingLoad.js related files

--- src/app/App.js
+++ src/app/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './../features/auth/Login';
 import Signup from './../features/auth/Signup';
 import AddTeachingLoad from './../features/teachingload/AddTeachingLoad';
+import EditTeachingLoad from './../features/teachingload/EditTeachingLoad';
 import AddStudyLoad from './../features/studyload/AddStudyLoad';
 import EditStudyLoad from './../features/studyload/EditStudyLoad';
 
@@ -18,6 +19,7 @@ class App extends Component {
             <Route exact={true} path="/" component={Login}/>
             <Route exact={true} path="/signup" component={Signup}/>
             <Route exact={true} path="/teachingload/add" component={AddTeachingLoad}/>
+            <Route exact={true} path="/teachingload/edit" component={EditTeachingLoad}/>
             <Route exact={true} path="/studyload/add" component={AddStudyLoad}/>
             <Route exact={true} path="/studyload/edit" component={EditStudyLoad}/>
           </div>
