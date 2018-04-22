f657d5c2b02cd70aa21e01e129084a89407b9683
kcferia <30889261+kcferia@users.noreply.github.com>
2018-04-23 01:06:55 +0800

    changed regex

--- src/features/publications/EditPublications.js
+++ src/features/publications/EditPublications.js
@@ -46,7 +46,7 @@ const errorTexts = [
   <span style={error}> {' *required'}</span> //8
 ];
 
-const alphanumRegex =  /^[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9]*$/;
+const alphanumRegex =  /^[a-zA-Z0-9 ]*[a-zA-Z ][a-zA-Z0-9 ]*$/;
 const numRegex = /^[0-9]+$/;
 
 var formError = {
